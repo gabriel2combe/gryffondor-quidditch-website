2983c08e29739a34d58b17876c517d81f877cc37
gabriel2combe <gabriel.decombe@orange.fr>
2018-05-03 09:43:07 +0200

    Fixe #71

--- public/assets/js/editPresentation.js
+++ public/assets/js/editPresentation.js
@@ -1,42 +1,12 @@
-function readURL1(input) {
+function readURL(input) {
     if (input.files && input.files[0]) {
-        var reader = new FileReader();
+        let reader = new FileReader();
 
         reader.onload = function (e) {
-            $('#picture1-pic')
-                .attr('src', e.target.result);
-            document.getElementById("picture1-title").innerHTML = "Nouvelle photo :";
+            $(input).siblings("img").attr('src', e.target.result);
+            $(input).siblings("h4").html("Nouvelle photo :");
         };
 
         reader.readAsDataURL(input.files[0]);
     }
 }
-
-function readURL2(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $('#picture2-pic')
-                .attr('src', e.target.result);
-            document.getElementById("picture2-title").innerHTML = "Nouvelle photo :";
-        };
-
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-
-function readURL3(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $('#picture3-pic')
-                .attr('src', e.target.result);
-            document.getElementById("picture3-title").innerHTML = "Nouvelle photo :";
-        };
-
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-
